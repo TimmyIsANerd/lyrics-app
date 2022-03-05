c80fe134a9c598125d6aa05a78fdedf540c21a43
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-03-05 22:02:13 +0100

    query lyrics endpoint

--- pages/api/lyrics.ts
+++ pages/api/lyrics.ts
@@ -1,23 +1,27 @@
 import axios from "axios";
-import { NextApiRequest, NextApiResponse } from "next";
 import {
     AxiosRequestConfig
 } from "axios";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const options: AxiosRequestConfig = {
-        method: "GET",
-        url: `https://genius-song-lyrics1.p.rapidapi.com/songs${req.query.id}`,
-        headers: {
-            'x-rapidapi-host': 'genius-song-lyrics1.p.rapidapi.com',
-            'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY
-        }
-    };
+export default async function handler(req: any, res: any) {
+    if (req.method === 'GET') {
+        const options: AxiosRequestConfig = {
+            method: "GET",
+            url: `https://genius-song-lyrics1.p.rapidapi.com/songs/${req.query.id}/lyrics`,
+            headers: {
+                'x-rapidapi-host': 'genius-song-lyrics1.p.rapidapi.com',
+                'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY,
+            }
+        };
 
-    try {
-        let response = await axios(options);
-        return res.status(200).json(response.data);
-    } catch (error) {
-        console.log(error);
+        axios.request(options)
+            .then((response) => {
+                return res.status(200).json(response.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    } else {
+        return res.status(400)
     }
 }
