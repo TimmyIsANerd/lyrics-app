0c4d276e382f184d661193034864c65ba4d5cca8
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-03-05 22:04:16 +0100

    made change to title

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,7 +52,7 @@ const Home: NextPage = () => {
     <div className="flex flex-col md:px-12 px-0 relative bg-background font-poppins items-center min-h-screen
     ">
       <h1 className="text-6xl font-bold text-primary mt-10">
-        <span className="text-active">Lyrics</span> App
+        Rapid <span className="text-active">Lyrics</span> Search App
       </h1>
 
       <h2 className="text-primary text-2xl font-light mt-6">Get the complete lyrics of any given track</h2>
