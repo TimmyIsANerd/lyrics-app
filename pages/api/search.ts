d2b0ea14feeb9e7e155692169a15c4f2f9230d3b
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2022-03-05 22:02:25 +0100

    query song search endpoint

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -6,20 +6,24 @@ import axios from "axios";
 
 export default async function handler(req:any,res:any){
 
-    const options:AxiosRequestConfig = {
-        method: "GET",
-        url: "https://genius-song-lyrics1.p.rapidapi.com/search",
-        params: {q: req.query.title, per_page: "10", page: "1"},
-        headers:{
-            'x-rapidapi-host': 'genius-song-lyrics1.p.rapidapi.com',
-            'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY
-        }
-    };
-    
-    try{
-        let response = await axios(options);
-        return res.status(200).json(response.data);
-    } catch (error){
-        console.log(error)
+    if(req.method === "GET"){
+        const options:AxiosRequestConfig = {
+            method: "GET",
+            url: "https://genius-song-lyrics1.p.rapidapi.com/search",
+            params: {q: req.query.title, per_page: "10", page: "1"},
+            headers:{
+                'x-rapidapi-host': 'genius-song-lyrics1.p.rapidapi.com',
+                'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPIDAPI_KEY
+            }
+        };
+
+        axios.request(options)
+        .then((response) =>{
+            return res.status(200).json(response.data);
+        })
+        .catch(err => console.log(err))
+    } else {
+        return res.status(400)
     }
+
 }
